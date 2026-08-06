e02715292672493c55800777f7a9214ce5d2c615
kp5331 <kpj5331@gmail.com>
2026-08-06 02:10:31 -0600

    Add functions to lock PC and take screenshots

--- pc_agent/pc_agent/actions.py
+++ pc_agent/pc_agent/actions.py
@@ -1,1 +1,22 @@
+import os
+import pyautogui
+from datetime import datetime
 
+
+def lock_pc():
+    os.system("rundll32.exe user32.dll,LockWorkStation")
+    return {
+        "status": "PC locked"
+    }
+
+
+def take_screenshot():
+    filename = f"screenshot_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+
+    image = pyautogui.screenshot()
+    image.save(filename)
+
+    return {
+        "status": "Screenshot saved",
+        "file": filename
+    }
