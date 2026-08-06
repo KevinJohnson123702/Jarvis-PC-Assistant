3ad9f45c5bd2b384e1b27ac3b3356ca59fe3acc8
kp5331 <kpj5331@gmail.com>
2026-08-06 03:27:13 -0600

    Update main.py

--- pc_agent/main.py
+++ pc_agent/main.py
@@ -1,10 +1,30 @@
 import uvicorn
+import threading
+import time
 
-if __name__ == "__main__":
-    print("Jarvis is starting...")
 
-    uvicorn.run(
-        "server:app",
-        host="0.0.0.0",
-        port=8000
-    )
+def start_voice():
+    import voice
+
+
+def run_voice():
+    time.sleep(2)
+    start_voice()
+
+
+print("Jarvis is starting...")
+
+
+voice_thread = threading.Thread(
+    target=run_voice,
+    daemon=True
+)
+
+voice_thread.start()
+
+
+uvicorn.run(
+    "server:app",
+    host="0.0.0.0",
+    port=8000
+)
